0b7df52dfa9c021b12d0e974dfb9486453baeb76
evrardco <colin.evrard.134@gmail.com>
2024-09-12 17:31:47 +0200

    add test for multiple values

--- tests/tests.py
+++ tests/tests.py
@@ -8,7 +8,9 @@ import os
 cwd = os.path.dirname(os.path.realpath(__file__))
 
 class ParseTest(unittest.TestCase):
-    def test_single_val(self):
+    def test_var_list(self):
         self.assertEqual(parse_var_list("test"), ["test"])
+        in1 = "PATH,LD_LIBRARY_PATH"
+        self.assertEqual(parse_var_list(in1), ["PATH", "LD_LIBRARY_PATH"])
 if __name__ == '__main__':
     unittest.main()
