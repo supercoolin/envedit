54f9e6ae7ec75c2ef560a4ec276d6fca3c19087f
evrardco <colin.evrard.134@gmail.com>
2024-09-12 17:51:31 +0200

    add test for parse_path_list

--- tests/tests.py
+++ tests/tests.py
@@ -2,15 +2,23 @@ import unittest
 import sys
 import os
 from context import *
-from envedit.parsing import parse_var_list
+from envedit.parsing import parse_var_list, parse_path_list
 
 import os
 cwd = os.path.dirname(os.path.realpath(__file__))
 
 class ParseTest(unittest.TestCase):
+
     def test_var_list(self):
         self.assertEqual(parse_var_list("test"), ["test"])
         in1 = "PATH,LD_LIBRARY_PATH"
         self.assertEqual(parse_var_list(in1), ["PATH", "LD_LIBRARY_PATH"])
+
+    def test_path_list(self):
+        self.assertEqual(parse_path_list("test"), ["test"])
+        in1 = "/dev/null:/tmp"
+        self.assertEqual(parse_path_list(in1), ["/dev/null", "/tmp"])
+
+
 if __name__ == '__main__':
     unittest.main()
