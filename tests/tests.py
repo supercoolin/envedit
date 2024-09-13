17eed04fe2110a65d69dec10eafe5ad5399cfb99
evrardco <colin.evrard.134@gmail.com>
2024-09-13 18:45:50 +0200

    Add tests for common

--- tests/tests.py
+++ tests/tests.py
@@ -3,6 +3,7 @@ import sys
 import os
 from context import *
 from envedit.parsing import parse_var_list, parse_path_list
+from envedit.common import path_list_to_str
 
 import os
 cwd = os.path.dirname(os.path.realpath(__file__))
@@ -18,6 +19,14 @@ class ParseTest(unittest.TestCase):
         self.assertEqual(parse_path_list("test"), ["test"])
         in1 = "/dev/null:/tmp"
         self.assertEqual(parse_path_list(in1), ["/dev/null", "/tmp"])
+
+class CommonTest(unittest.TestCase):
+
+    def test_path_list_to_str(self):
+        
+        self.assertEqual(path_list_to_str(["/dev/null", "/tmp"]), "/dev/null:/tmp")
+        self.assertEqual(path_list_to_str(["root"]), "root")
+        self.assertEqual(path_list_to_str([]), "")
 
 
 if __name__ == '__main__':
