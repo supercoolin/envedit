ae0b56aecd3788b1400a19983aac45e3633eeaf5
evrardco <colin.evrard.134@gmail.com>
2024-09-13 18:23:10 +0200

    Add code for converting path list to string

--- envedit/common.py
+++ envedit/common.py
@@ -2,3 +2,11 @@ from typing import List, Tuple, Dict, Union
 #types
 var_list_t = List[str]
 path_list_t = List[str]
+#functions 
+
+def path_list_to_str(path: path_list_t)->str:
+    """ convert path list to string
+    @param path: path list
+    @return: string
+    """
+    return ":".join(path)
