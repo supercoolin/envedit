881971ca6060567343827c9c0dbf82213645915a
evrardco <colin.evrard.134@gmail.com>
2024-09-28 18:43:00 +0200

    v1.1.0

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ setup(
     ],
     classifiers=[
         "Programming Language :: Python :: 3",
-        "License :: MIT License",
+        "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
     python_requires=">=3.10",
